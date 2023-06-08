e12a8edc48ed374857c3ed9dffdce0af42ef4968
Little-Ki <51691340+Little-Ki@users.noreply.github.com>
2023-06-08 12:12:12 +0800

    Update json_parser.hpp

--- json_parser.hpp
+++ json_parser.hpp
@@ -9,332 +9,289 @@
 
 namespace json {
 
-  class parser {
-  private:
-  
-    #define SKIP_CHECK { skip_space(); if (eof()) return false; }
-  public:
-    parser(const std::string& doc) : m_doc(doc) {}
+	class parser {
+	private:
+		#define SKIP_CHECK { skip_space(); if (eof()) return false; }
 
-    bool parse() {
-      auto succ = parse_t();
-      if (!succ) {
-        size_t line = 0, col = 0;
-        size_t old_pos = m_pos;
-        for(auto i = 0; i < old_pos;) {
-          if (m_doc[i] == '\n') {
-            line++;
-            col = 0;
-          } else col++;
-          i += utf8_len();
-          m_pos = i;
+	public:
+		parser(const std::string& doc) : m_doc(doc) {}
+
+		bool parse() {
+			auto succ = parse_t();
+			if (!succ) {
+				std::cout << "Parse json failed at line:" << m_line << ", col:" << m_col << '\n';
+			}
+			return succ;
+		}
+
+		json::value document() {
+			return m_json;
+		}
+
+		bool valid() {
+			return m_json.type() != TYPE::INVALID;
+		}
+
+	private:
+		
+		bool parse_t() {
+			m_pos = 0;
+      m_line = 0;
+      m_col = 0;
+			if (!parse_value(m_json)) {
+				return false;
+			}
+			return true;
+		}
+
+		bool parse_value(json::value& out) {
+			SKIP_CHECK;
+			if (peek() == '[') { 
+					out = json::value(json::TYPE::ARRAY);
+					if (!parse_array(out)) return false;
+			}
+			else if (peek() == '{') { 
+					out = json::value(json::TYPE::DIRCTORY);
+					if (!parse_object(out)) return false;
+			}
+			else if (peek() == '\"') { 
+					std::string str;
+					if (!parse_string(str)) return false;
+					out = str;
+			}
+			else if (peek() >= '0' && peek() <= '9'){ 
+					std::string str;
+					bool f, e;
+					if (!parse_number(str, f, e)) return false;
+					if (f) 
+						out = std::stod(str);
+					else 
+						out = std::stoi(str);
+			}
+			else if (peek() == 't' || peek() == 'f') {
+				bool val = false;
+				if (!parse_boolean(val)) return false;
+				out = val;
+			}
+			else if (peek() == 'n') {
+				if (!parse_null()) return false;
+				out.set_null();
+			}
+			return true;
+		}
+
+		bool parse_object(json::value& out) {
+			if (peek_next() != '{') return false;
+			out = value::dic_t();
+
+			while(!eof() && peek() != '}') {
+				SKIP_CHECK;
+				if (peek() != '\"') return false;
+				std::string key;
+				auto succ = parse_string(key);
+				if (!succ) return false;
+				SKIP_CHECK;
+				if (peek_next() != ':') return false;
+				SKIP_CHECK;
+				json::value val;
+				if (!parse_value(val)) {
+					return false;
+				}
+				out[key] = val;
+				SKIP_CHECK;
+				if (peek() == ',') {
+					next();
+				}
+			}
+			if (eof()) return false;
+			if (peek() != '}') { return false; }
+			next();
+			return true;
+		}
+
+		bool parse_array(json::value& out) {
+			if (peek_next() != '[') return false;
+			out = value::arr_t();
+
+			while(!eof() && peek() != ']') {
+				SKIP_CHECK;
+				json::value val;
+				if (!parse_value(val)) {
+					return false;
+				}
+				out += val;
+				SKIP_CHECK;
+				if (peek() == ',') {
+					next();
+				}
+			}
+			if (eof()) return false;
+			if (peek() != ']') return false;
+			next();
+			return true;
+		}
+
+		bool parse_string(std::string& out) {
+			size_t len = 0;
+			auto c = peek_next();
+			if (c != '\"') {
+				return false;
+			}
+			c = peek();
+			while(c != '\"' && !eof()) {
+				if (c == '\\') {
+					next();
+					char next = peek_next();
+					if (next == '/')	out.push_back('/');
+					if (next == '\\') out.push_back('\\');
+					if (next == 'b')	out.push_back('\b');
+					if (next == 'f')	out.push_back('\f');
+					if (next == 't')	out.push_back('\t');
+					if (next == 'n')	out.push_back('\n');
+					if (next == 'r')	out.push_back('\r');
+					if (next == '"')	out.push_back('\"');
+					if (next == 'u') {
+						char h0 = peek_next();
+						char h1 = peek_next();
+						char h2 = peek_next();
+						char h3 = peek_next();
+						if (!is_hex(h0) || !is_hex(h1) || !is_hex(h2) || !is_hex(h3)) {
+							return false;
+						}
+						auto unicode = (h0 << 12) | (h1 << 8)	| (h2 << 4) | h3;
+						std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> converter;
+						std::string u8str = converter.to_bytes(unicode);
+						out.append(u8str);
+					};
+					c = peek();
+				} else {
+					auto size = utf8_len();
+					while(size--) {
+						out.push_back(peek_next());
+					}
+				}
+				c = peek();
+			}
+			if (peek() != '\"') return false;
+			next();
+			return true;
+		}
+
+		bool parse_number(std::string& out, bool& f, bool& e) {
+			size_t dot = 0, _e = 0;
+			auto c = peek();
+			bool valid = false;
+			if (!(c >= '0' && c <= '9')) { return false; }
+			while ((c >= '0' && c <= '9') || c == '.' || c == 'e') {
+				out.push_back(c);
+				if (c == '.') {
+					valid = false;
+					if (dot != 0) {
+						break;
+					}
+					else dot = m_pos;
+				}
+				if (c == 'e') {
+					valid = false;
+					if (_e != 0)	{
+						break;
+					}
+					else _e = m_pos;
+				}
+				if (c >= '0' && c <= '9') {
+					valid = true;
+				}
+				next();
+				c = peek();
+			};
+			f = dot != 0;
+			e = _e != 0;
+			return valid && ((dot != 0 && _e != 0) ? (_e > dot && _e - dot > 1) : true);
+		}
+
+		bool parse_boolean(bool& out) {
+			auto c = peek_next();
+			if (c != 't' && c != 'f') return false;
+			if (c == 't') {
+				if (peek_next() != 'r') return false;
+				if (peek_next() != 'u') return false;
+				if (peek_next() != 'e') return false;
+				out = true;
+			}
+			if (c == 'f') {
+				if (peek_next() != 'a') return false;
+				if (peek_next() != 'l') return false;
+				if (peek_next() != 's') return false;
+				if (peek_next() != 'e') return false;
+				out = false;
+			}
+			return true;
+		}
+
+		bool parse_null() {
+			auto c = peek_next();
+			if (c != 'n') return false;
+			if (peek_next() != 'u') return false;
+			if (peek_next() != 'l') return false;
+			if (peek_next() != 'l') return false;
+			return true;
+		}
+
+		void skip_space() {
+			auto c = peek();
+			while(
+				!eof() && 
+				(c == ' ' || c == '\n' || c == '\t' || c == '\r')
+			) {
+        if (c == '\n') {
+          m_col = 0;
+          m_line += 1;
         }
-        m_pos = old_pos;
-        std::cout << "Parse json failed at line:" << line << ", col:" << col << '\n';
-      }
-      return succ;
-    }
+				next();
+				c = peek();
+			}
+		}
 
-    json::value document() {
-      return m_result;
-    }
+		bool eof() { return m_pos == m_doc.size(); }
 
-    bool valid() {
-      return m_result.type() != VALTYPE::UNKNOWN;
-    }
+		char peek() { return m_doc[m_pos]; }
 
-  private:
-    
-    bool parse_t() {
-      
-      m_pos = 0;
-      SKIP_CHECK;
+		char peek_next() { auto c = peek(); next(); return c; }
 
-      /**/ if (peek() == '[') { 
-          m_result = json::value(value::arr_t());
-          if (!parse_array(m_result)) return false;
-      }
-      else if (peek() == '{') { 
-          m_result = json::value(value::dic_t());
-          if (!parse_object(m_result)) return false;
-      }
-      else if (peek() == '\"') { 
-          std::string str;
-          if (!parse_string(str)) return false;
-          m_result = json::value(str);
-      }
-      else if (peek() >= '0' && peek() <= '9'){ 
-          std::string str;
-          bool floating;
-          if (!parse_number(str, floating)) return false;
-          m_result = floating ? json::value(std::stod(str)) : json::value(std::stoi(str));
-      }
-      else if (peek() == 't' || peek() == 'f') {
-        bool result = false;
-        if (!parse_boolean(result)) return false;
-        m_result = json::value(result);
-      }
-      else if (peek() == 'n') {
-        if (!parse_null()) return false;
-        m_result = json::value();
-      }
-      return true;
-    }
+		void next() { m_pos += 1; m_col += 1;}
 
-    bool parse_object(json::value& out) {
-      if (peek_next() != '{') return false;
-      while(!eof() && peek() != '}') {
-        SKIP_CHECK;
-        if (peek() != '\"') return false;
+		size_t utf8_len() {
+			auto d = (const unsigned char*)m_doc.data();
+			auto c = d[m_pos];
+			/**/ if ((c & 0xF8) == 0xF0) return 4U;
+			else if ((c & 0xF0) == 0xE0) return 3U; 
+			else if ((c & 0xE0) == 0xC0) return 2U;
+			else if ((c & 0x80) == 0x00) return 1U;
+			return 1U;
+		}
+		
+		uint8_t hex2dec(char val) {
+			if (val >= '0' && val <= '9') return val - '0';
+			if (val >= 'a' && val <= 'f') return val - 'a' + 10;
+			if (val >= 'A' && val <= 'F') return val - 'A' + 10;
+			return 0;
+		}
 
-        std::string key;
-        auto succ = parse_string(key);
-        if (!succ) return false;
+		bool is_hex(char val) {
+			return 
+			(val >= '0' && val <= '9') || 
+			(val >= 'a' && val <= 'f') ||
+			(val >= 'A' && val <= 'F') ;
+		}
 
-        SKIP_CHECK;
-        if (peek_next() != ':') return false;
-        SKIP_CHECK;
-        
-        /**/ if (peek() == '{') {
-          auto tmp = json::value(value::dic_t());
-          if (!parse_object(tmp)) return false;
-          out.set(key, tmp);
-        }
-        else if (peek() == '[') {
-          auto tmp = json::value(value::arr_t());
-          if (!parse_array(tmp)) return false;
-          out.set(key, tmp);
-        }
-        else if (peek() == '\"') {
-          std::string str;
-          if (!parse_string(str)) return false;
-          out.set(key, json::value(str));
-        }
-        else if (peek() >= '0' && peek() <= '9') {
-          std::string str;
-          bool floating;
-          if (!parse_number(str, floating)) return false;
-          out.set(key, floating ? json::value(std::stod(str)) : json::value(std::stoi(str)));
-        }
-        else if (peek() == 't' || peek() == 'f') {
-          bool result = false;
-          if (!parse_boolean(result)) return false;
-          out.set(key, json::value(result));
-        }
-        else if (peek() == 'n') {
-          if (!parse_null()) return false;
-          out.set(key, json::value());
-        }
+	public:
+		std::string m_doc;
 
-        SKIP_CHECK;
-        if (peek() == ',') {
-          next();
-        }
-      }
-      if (eof()) return false;
-      if (peek() != '}') { return false; }
-      next();
-      return true;
-    }
+		size_t m_pos { 0 };
 
-    bool parse_array(json::value& out) {
-      if (peek_next() != '[') return false;
-      while(!eof() && peek() != ']') {
-        SKIP_CHECK;
-        /**/ if (peek() == '{') {
-          auto tmp = json::value(value::dic_t());
-          if (!parse_object(tmp)) return false;
-          out.append(tmp);
-        }
-        else if (peek() == '[') {
-          auto tmp = json::value(value::arr_t());
-          if (!parse_array(tmp)) return false;
-          out.append(tmp);
-        }
-        else if (peek() == '\"') {
-          std::string str;
-          if (!parse_string(str)) return false;
-          out.append(json::value(str));
-        }
-        else if (peek() >= '0' && peek() <= '9') {
-          std::string str;
-          bool floating;
-          if (!parse_number(str, floating)) return false;
-          out.append(floating ? json::value(std::stod(str)) : json::value(std::stoi(str)));
-        }
-        else if (peek() == 't' || peek() == 'f') {
-          bool result = false;
-          if (!parse_boolean(result)) return false;
-          out.append(json::value(result));
-        }
-        else if (peek() == 'n') {
-          if (!parse_null()) return false;
-          out.append(json::value());
-        }
-        SKIP_CHECK;
-        if (peek() == ',') {
-          next();
-        }
-      }
-      if (eof()) return false;
-      if (peek() != ']') return false;
-      next();
-      return true;
-    }
+    size_t m_line { 0 }, m_col { 0 };
 
-    bool parse_string(std::string& out) {
-      size_t len = 0;
-      auto c = peek_next();
-      if (c != '\"') {
-        return false;
-      }
-      c = peek();
-      while(c != '\"' && !eof()) {
-        if (c == '\\') {
-          next();
-          char next = peek_next();
-          if (next == '/')  out.push_back('/');
-          if (next == '\\') out.push_back('\\');
-          if (next == 'b')  out.push_back('\b');
-          if (next == 'f')  out.push_back('\f');
-          if (next == 't')  out.push_back('\t');
-          if (next == 'n')  out.push_back('\n');
-          if (next == 'r')  out.push_back('\r');
-          if (next == '"')  out.push_back('\"');
-          if (next == 'u') {
-            char h0 = peek_next();
-            char h1 = peek_next();
-            char h2 = peek_next();
-            char h3 = peek_next();
-            if (!is_hex(h0) || !is_hex(h1) || !is_hex(h2) || !is_hex(h3)) {
-              return false;
-            }
-            auto unicode = (h0 << 12) | (h1 << 8)  | (h2 << 4) | h3;
-            std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> converter;
-            std::string u8str = converter.to_bytes(unicode);
-            out.append(u8str);
-          };
-          c = peek();
-        } else {
-          auto size = utf8_len();
-          while(size--) {
-            out.push_back(peek_next());
-          }
-        }
-        c = peek();
-      }
-      if (peek() != '\"') return false;
-      next();
-      return true;
-    }
+		json::value m_json;
 
-    bool parse_number(std::string& out, bool& floating) {
-      size_t dot = 0, e = 0;
-      auto c = peek();
-      bool valid = false;
-      if (!(c >= '0' && c <= '9')) { return false; }
-      while ((c >= '0' && c <= '9') || c == '.' || c == 'e') {
-        out.push_back(c);
-        if (c == '.') {
-          valid = false;
-          if (dot != 0) {
-            break;
-          }
-          else dot = m_pos;
-        }
-        if (c == 'e') {
-          valid = false;
-          if (e != 0)  {
-            break;
-          }
-          else e = m_pos;
-        }
-        if (c >= '0' && c <= '9') {
-          valid = true;
-        }
-        next();
-        c = peek();
-      };
-      floating = dot != 0;
-      return valid && ((dot != 0 && e != 0) ? (e > dot && e - dot > 1) : true);
-    }
-
-    bool parse_boolean(bool& out) {
-      auto c = peek_next();
-      if (c != 't' && c != 'f') return false;
-      if (c == 't') {
-        if (peek_next() != 'r') return false;
-        if (peek_next() != 'u') return false;
-        if (peek_next() != 'e') return false;
-        out = true;
-      }
-      if (c == 'f') {
-        if (peek_next() != 'a') return false;
-        if (peek_next() != 'l') return false;
-        if (peek_next() != 's') return false;
-        if (peek_next() != 'e') return false;
-        out = false;
-      }
-      return true;
-    }
-
-    bool parse_null() {
-      auto c = peek_next();
-      if (c != 'n') return false;
-      if (peek_next() != 'u') return false;
-      if (peek_next() != 'l') return false;
-      if (peek_next() != 'l') return false;
-      return true;
-    }
-
-    void skip_space() {
-      auto c = peek();
-      while(
-        !eof() && 
-        (c == ' ' || c == '\n' || c == '\t' || c == '\r')
-      ) {
-        next();
-        c = peek();
-      }
-    }
-
-    bool eof() { return m_pos == m_doc.size(); }
-
-    char peek() { return m_doc[m_pos]; }
-
-    char peek_next() { auto c = peek(); next(); return c; }
-
-    void next() { m_pos += 1; }
-
-    size_t utf8_len() {
-      auto d = (const unsigned char*)m_doc.data();
-      auto c = d[m_pos];
-      /**/ if ((c & 0xF8) == 0xF0) return 4U;
-      else if ((c & 0xF0) == 0xE0) return 3U; 
-      else if ((c & 0xE0) == 0xC0) return 2U;
-      else if ((c & 0x80) == 0x00) return 1U;
-      return 1U;
-    }
-    
-    uint8_t hex2dec(char val) {
-      if (val >= '0' && val <= '9') return val - '0';
-      if (val >= 'a' && val <= 'f') return val - 'a' + 10;
-      if (val >= 'A' && val <= 'F') return val - 'A' + 10;
-      return 0;
-    }
-
-    bool is_hex(char val) {
-      return 
-      (val >= '0' && val <= '9') || 
-      (val >= 'a' && val <= 'f') ||
-      (val >= 'A' && val <= 'F') ;
-    }
-
-  public:
-    std::string m_doc;
-
-    size_t m_pos = 0;
-
-    json::value m_result;
-  };
+	};
 }
